e262698970e3e7749aa2b42485db59a5782db341
simpleframework <cknet@126.com>
2013-11-15 12:17:41 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/module/myportal/impl/LayoutLobService.java
+++ src/main/java/net/simpleframework/module/myportal/impl/LayoutLobService.java
@@ -6,8 +6,7 @@ import net.simpleframework.module.myportal.LayoutLobBean;
 /**
  * Licensed under the Apache License, Version 2.0
  * 
- * @author 陈侃(cknet@126.com, 13910090885)
- *         https://github.com/simpleframework
+ * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
 public class LayoutLobService extends AbstractPortalService<LayoutLobBean> implements
